050c04beafa1842735bd4ab6c1123c7dc5e3f900
URUETA CANTILLO JESUS <jurueta@cuc.edu.co>
2018-08-13 10:58:45 -0500

    Cambio y agregar push

--- PracticaPilas/src/Pilas/Ejecutable.java
+++ PracticaPilas/src/Pilas/Ejecutable.java
@@ -15,6 +15,7 @@ public class Ejecutable {
         pila.push(1);
         pila.push(2);
         pila.push(3);
+        pila.push(5);
         System.out.println(pila.peek());
         System.out.println(pila);
     }
